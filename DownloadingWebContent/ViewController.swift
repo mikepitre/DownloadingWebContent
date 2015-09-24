9d81d127280fa95e1839e4f732f130ecc1bd1ff7
mikepitre <michaelepitre@gmail.com>
2015-09-23 19:32:12 -0500

    removed web content display only and used downloaded data

--- DownloadingWebContent/ViewController.swift
+++ DownloadingWebContent/ViewController.swift
@@ -17,30 +17,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         let url = NSURL(string: "http://www.stackoverflow.com")!
-        
-        webView.loadRequest(NSURLRequest(URL: url))
 
-//        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
-//            
-//            //Will happen when task completes
-//            
-//            if let urlContent = data {
-//                
-//                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
-//                
-//                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-//                    
-//                    self.webView.loadHTMLString(String(webContent), baseURL: nil)
-//                    
-//                })
-//                
-//                
-//            }
-//            
-//        }
-//        
-//        task.resume()
-//        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
+            
+            //Will happen when task completes
+            
+            if let urlContent = data {
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                print(webContent)
+                
+                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                    
+                    self.webView.loadHTMLString(String(webContent), baseURL: nil)
+                    
+                })
+                
+                
+            }
+            
+        }
+        
+        task.resume()
+        
     }
 
     override func didReceiveMemoryWarning() {
